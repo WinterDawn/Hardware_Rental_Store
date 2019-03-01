d21b20ba2a399b9e11b944275900aa42fd427991
dowu6943 <dowu6943@colorado.edu>
2019-03-01 02:19:40 -0700

    modify Store

--- Store.py
+++ Store.py
@@ -1,34 +1,54 @@
+class Rental:
+    pass
+
 class Store:
     def __init__(self, Name):
         self.name = Name
         self.Tools = []
-        self.rental = {}
-        self.initialrental()
-        self.money = 0
+        self.rental = []
+        self.income = 0
+
     def isempty(self):
         if len(self.Tools)==0:
             return True
         else:
             return False
+
     def addTools(self, tool):
         self.Tools.append(tool)
+
     def removeTools(self, tool):
         self.Tools.remove(tool)
-    def ToolsInStore(self):
-        return len(self.Tools)
-    def showTools(self):
-        for i in self.Tools:
-            print(i)
-    def initialrental(self):
-        for i in range(10):
-            self.rental[i] = []
-    def rent(self, customerid, tool, days):
-        temp = [tool, days]
-        self.rental[customerid].append(temp)
-        self.removeTools(tool)
-        self.money = self.money + tool.get_price()*days
 
-    def give_back(self, customerid, tool):
+    def num_tools(self):
+        return len(self.Tools)
+
+    def get_tools(self):
+        return self.Tools
+
+    def get_rental(self):
+        return self.rental
+    
+    def get_income(self):
+        return self.income
+
+    # def initialrental(self):
+    #     for i in range(10):
+    #         self.rental[i] = []
+
+    def rent(self, customer, tool, days):
+        payment = tool.get_price()*days
+        self.income = self.income + payment
+        record = {"customer":customer,"tool":tool,"nights": days,"price": payment, "active": True}
+        self.rental.append(record)
+        self.removeTools(tool)
+        
+
+    def give_back(self, customer, tool):
         self.addTools(tool)
-        self.rental[customerid].remove([tool, 0])
+        for r in self.rental:
+            if (r["customer"] == customer and
+                r["tool"] == tool and
+                r["active"]):
+                r["active"] = False
 
