e70edb622d78e3f8c2baa8e3833044a6c1a3ec2e
zzihan1206 <zizh1583@colorado.edu>
2019-02-28 20:25:15 -0700

    added customer

--- Customers.py
+++ Customers.py
@@ -1,28 +1,33 @@
-class Customer(object):
+from abc import ABC,abstractmethod;
+
+class Customer(ABC):
+
+
 	def __init__(self,name,does_rent,rent_time):
 		self.name=name
 		self.does_rent=does_rent
 		self.rent_time=rent_time
 
-	def get_name(self):
+	def get_name(self，name):
 		return self.name
 
-	def get_does_rent(self):
+	def get_does_rent(self,does_rent):
 		return self.does_rent
 
-	def get_rent_time(self):
+	def get_rent_time(self,rent_time):
 		return self.rent_time
 
-	def rent(self):
+	def rent(self,does_rent):
 		does_rent= True
+	def returned(self,does_rent):
+		does_rent= False
+
 
 
 class Casual(Customer):
 	pass
-
 class Business(Customer):
 	pass
-
 class Regular(Customer):
 	pass
 
